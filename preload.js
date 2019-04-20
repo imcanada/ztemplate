75e80bc1affa9e3ef5ee75cb8d3fac3f64c48e6a
imcanada <47786934+imcanada@users.noreply.github.com>
2019-04-20 17:40:46 +0000

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,12 @@
+<!--//--><![CDATA[//><!--
+			var images = new Array()
+			function preload() {
+				for (i = 0; i < preload.arguments.length; i++) {
+					images[i] = new Image()
+					images[i].src = preload.arguments[i]
+				}
+			}
+			preload(
+				"https://zreddx.net/sht.jpg",
+			)
+		//--><!]]>
